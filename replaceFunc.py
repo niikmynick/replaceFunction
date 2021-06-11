d21d73e551c5f21c02b749bb22105872353f1218
Nikita <76608743+niikmynick@users.noreply.github.com>
2021-06-11 10:51:49 +0300

    Add files via upload

---
+++ replaceFunc.py
@@ -0,0 +1,31 @@
+placesMas = []
+n = int(input('amount of files:\n'))
+for k in range(n):
+    placesMas.append(input('file {}\n'.format(k + 1)))
+
+# C:\Users\fk724s\OneDrive\Документы\files\Programming\replaceFuncInfo\firstDoc.txt
+# C:\Users\fk724s\OneDrive\Документы\files\Programming\replaceFuncInfo\secondDoc.txt
+# C:\Users\fk724s\OneDrive\Документы\files\Programming\replaceFuncInfo\thirdDoc.txt
+
+a = input('string (type in format: old line-new line)\n').split('-')
+wordInput = a[0]
+newWordInput = a[1]
+
+
+def replace(places, word, newWord):
+    try:
+        for i in places:
+            with open(i, 'r') as file:
+                oldData = file.read()
+
+            new_data = oldData.replace(word, newWord)
+
+            with open(i, 'w') as file1:
+                file1.write(new_data)
+    except BaseException:
+        print('Failed')
+    else:
+        print('Success')
+
+
+replace(placesMas, wordInput, newWordInput)
